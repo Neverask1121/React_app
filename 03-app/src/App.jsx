ccfc18d7dd97ebf95b0357e3502fe2ff6d2f6e34
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-06 00:07:38 +0530

    Props have been partially done

--- 03-app/src/App.jsx
+++ 03-app/src/App.jsx
@@ -3,12 +3,11 @@ import Card from './components/Card.jsx'
 
 const App = () => {
   return (
-    <div class = "parent">
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+    <div className = "parent">
+      <Card user = "Bhandari" age = {18}/> {/**So this a comment and this is the first prop I created */}
+      {/**Is this a comment or what */}
+      {/**Props are also can be told as by the name properties */}
+      <Card user = "Alina" age = {21}/>
       
     </div>
   )
