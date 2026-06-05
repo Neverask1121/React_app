5ba29f1894f2dc6abda514ead54caee608a6b659
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-05 16:20:57 +0530

    Restructurin the files

---
+++ 02-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    // making the good use of fragments
+    <>
+      <div id = "parent">
+        <h1 id = "child">Hello</h1>
+        <h2 id = "child">Aditya here</h2>
+        <h3 id = "child">Hello</h3>
+      </div>
+      <div id = "chacha">Chacha</div>
+    </>
+  )
+}
+
+export default App
