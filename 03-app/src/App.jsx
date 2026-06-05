7d4776542657438e85082e11f38da8a0b500b4d8
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-05 19:22:30 +0530

    Adding card

--- 03-app/src/App.jsx
+++ 03-app/src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import Card from './components/Card.jsx'
 
 const App = () => {
   return (
     <div class = "parent">
-      <div className="card">
-        <img src ="https://images.unsplash.com/photo-1778100207121-2ee237397a43?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
-        <h1>Aditya Bhandari</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, rerum?</p>
-        <button>Touch me</button>
-      </div>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+      
     </div>
   )
 }
---
+++ 03-app/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+      <div className="card">
+        <img src ="https://images.unsplash.com/photo-1778100207121-2ee237397a43?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
+        <h1>Aditya Bhandari</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, rerum?</p>
+        <button>Touch me</button>
+      </div>
+  )
+}
+
+export default Card
