a981384b5b4cb319e1822fded2ee6ceea4230fd5
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-06 23:04:06 +0530

    Adding the final product without components

--- 04-app/src/App.jsx
+++ 04-app/src/App.jsx
@@ -1,30 +1,30 @@
 import React from 'react'
-import {Bookmark } from "lucide-react"
+import { Bookmark } from "lucide-react"
 
 const App = () => {
   return (
-    <div className = "parent">
+    <div className="parent">
       <div className="card">
-        <div className="top">
-          <img src='https://thumbs.dreamstime.com/b/amazon-logo-white-background-montreal-canada-july-printed-paper-98221126.jpg'></img>
-          <Bookmark/>
-        </div>
-        <div className="center">
-          <h3>Amazon <span>3 days ago</span></h3>
-          <h2>Senior UI/UX designer</h2>
-          <div>
-            <h4>Part Time</h4>
-            <h4>Remote</h4>
+        <div>
+          <div className="top">
+            <img src='https://thumbs.dreamstime.com/b/amazon-logo-white-background-montreal-canada-july-printed-paper-98221126.jpg'></img>
+            <button><Bookmark /> Save</button>
+          </div>
+          <div className="center">
+            <h3>Amazon <span>3 days ago</span></h3>
+            <h2>Senior UI/UX designer</h2>
+            <div className="tag">
+              <h4>Part Time</h4>
+              <h4>Remote</h4>
+            </div>
           </div>
         </div>
         <div className="bottom">
-          <div>
+          <div className="bottom-left">
             <h3>$100/hr</h3>
             <p>Hyderabad, India</p>
-            <div>
-              <button>Apply now</button>
-            </div>
           </div>
+          <button className="bottom-right">Apply now</button>
         </div>
       </div>
     </div>
