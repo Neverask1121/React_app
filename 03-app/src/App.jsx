c756ce00baef9573d878718f740f80891677b18f
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-06 14:36:27 +0530

    03-app submitting

--- 03-app/src/App.jsx
+++ 03-app/src/App.jsx
@@ -4,10 +4,10 @@ import Card from './components/Card.jsx'
 const App = () => {
   return (
     <div className = "parent">
-      <Card user = "Bhandari" age = {18}/> {/**So this a comment and this is the first prop I created */}
+      <Card user = "Bhandari" age = {18} img = "https://plus.unsplash.com/premium_photo-1666672388644-2d99f3feb9f1?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/> {/**So this a comment and this is the first prop I created */}
       {/**Is this a comment or what */}
       {/**Props are also can be told as by the name properties */}
-      <Card user = "Alina" age = {21}/>
+      <Card user = "Alina" age = {21} img = "https://plus.unsplash.com/premium_photo-1688740375397-34605b6abe48?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
       
     </div>
   )
--- 03-app/src/components/Card.jsx
+++ 03-app/src/components/Card.jsx
@@ -4,7 +4,7 @@ const Card = (receiver) => {
   return (
       console.log(receiver.user, receiver.age),
       <div className="card">
-        <img src ="https://images.unsplash.com/photo-1778100207121-2ee237397a43?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"></img>
+        <img src = {receiver.img}></img>
         <h1>{receiver.user}</h1>
         <h3>Age = {receiver.age}</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, rerum?</p>
