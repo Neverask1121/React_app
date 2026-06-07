1f16f62dd55d9a2bcdddb00fab8772923d36263f
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-07 11:44:47 +0530

    Adding the componenets to -04-app

--- 04-app/src/App.jsx
+++ 04-app/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Card from './components/Card'
+import User from './components/User'
 
 const App = () => {
+
+  const arr = [10, 20, 30]
+
   return (
     <div className="parent">
-      <Card/>
-      <Card/>
-      <Card/>
+      {arr.map(function(elem){
+        return <Card/>
+      })}
     </div>
   )
 }
