8a0227761b083ecbe4ff32a560293ea0472f9701
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-07 14:01:32 +0530

    Adding the 04-app to the jungle

--- 04-app/src/App.jsx
+++ 04-app/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
 
   const jobOpenings = [
     {
-      brandLogo: "https://logo.clearbit.com/google.com",
+      brandLogo: "https://pngimg.com/uploads/meta/meta_PNG12.png",
       company: "Google",
       datePosted: "5 days ago",
       tag1: "Full Time",
@@ -15,7 +15,7 @@ const App = () => {
       post: "Frontend Developer"
     },
     {
-      brandLogo: "https://logo.clearbit.com/meta.com",
+      brandLogo: "https://static.vecteezy.com/system/resources/previews/014/018/561/non_2x/amazon-logo-on-transparent-background-free-vector.jpg",
       company: "Meta",
       datePosted: "1 week ago",
       tag1: "Full Time",
@@ -25,7 +25,7 @@ const App = () => {
       post: "Software Engineer"
     },
     {
-      brandLogo: "https://logo.clearbit.com/apple.com",
+      brandLogo: "https://substackcdn.com/image/fetch/$s_!G1lk!,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F8ed3d547-94ff-48e1-9f20-8c14a7030a02_2000x2000.jpeg",
       company: "Apple",
       datePosted: "3 days ago",
       tag1: "Full Time",
@@ -35,7 +35,7 @@ const App = () => {
       post: "iOS Developer"
     },
     {
-      brandLogo: "https://logo.clearbit.com/amazon.com",
+      brandLogo: "https://images.ctfassets.net/4cd45et68cgf/Rx83JoRDMkYNlMC9MKzcB/2b14d5a59fc3937afd3f03191e19502d/Netflix-Symbol.png?w=700&h=456",
       company: "Amazon",
       datePosted: "10 weeks ago",
       tag1: "Part Time",
@@ -45,7 +45,7 @@ const App = () => {
       post: "Cloud Support Engineer"
     },
     {
-      brandLogo: "https://logo.clearbit.com/microsoft.com",
+      brandLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnOVjTWaheo4E99cgYZ6y14tpsgHlm0VN8Hw&s",
       company: "Microsoft",
       datePosted: "2 weeks ago",
       tag1: "Full Time",
@@ -55,7 +55,7 @@ const App = () => {
       post: "Backend Engineer"
     },
     {
-      brandLogo: "https://logo.clearbit.com/netflix.com",
+      brandLogo: "https://download.logo.wine/logo/Microsoft_Store/Microsoft_Store-Logo.wine.png",
       company: "Netflix",
       datePosted: "6 days ago",
       tag1: "Full Time",
@@ -65,7 +65,7 @@ const App = () => {
       post: "Platform Engineer"
     },
     {
-      brandLogo: "https://logo.clearbit.com/nvidia.com",
+      brandLogo: "https://blog.logomaster.ai/hs-fs/hubfs/ibm-logo-1967.jpg?width=672&height=454&name=ibm-logo-1967.jpg",
       company: "NVIDIA",
       datePosted: "4 days ago",
       tag1: "Full Time",
@@ -75,7 +75,7 @@ const App = () => {
       post: "AI Engineer"
     },
     {
-      brandLogo: "https://logo.clearbit.com/openai.com",
+      brandLogo: "https://www.pngplay.com/wp-content/uploads/13/Tesla-Logo-PNG-HD-Quality.png",
       company: "OpenAI",
       datePosted: "8 days ago",
       tag1: "Full Time",
@@ -85,7 +85,7 @@ const App = () => {
       post: "Machine Learning Engineer"
     },
     {
-      brandLogo: "https://logo.clearbit.com/uber.com",
+      brandLogo: "https://www.nvidia.com/content/dam/en-zz/Solutions/about-nvidia/logo-and-brand/nvidia-og-image-white-bg-1200x630.jpg",
       company: "Uber",
       datePosted: "12 days ago",
       tag1: "Part Time",
@@ -95,7 +95,7 @@ const App = () => {
       post: "React Developer"
     },
     {
-      brandLogo: "https://logo.clearbit.com/adobe.com",
+      brandLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnOVjTWaheo4E99cgYZ6y14tpsgHlm0VN8Hw&s",
       company: "Adobe",
       datePosted: "9 days ago",
       tag1: "Full Time",
@@ -108,8 +108,10 @@ const App = () => {
   console.log(jobOpenings);
   return (
     <div className="parent">
-      {jobOpenings.map(function(elem){
-        return <Card company = {elem.company}/>
+      {jobOpenings.map(function(elem, idx){
+        return <div key= {idx}>
+          <Card datePosted = {elem.datePosted} company = {elem.company} post = {elem.post} tag1 = {elem.tag1} tag2 = {elem.tag2} pay = {elem.pay} location = {elem.location} brand = {elem.brandLogo}/>
+          </div>
       })}
     </div>
   )
--- 04-app/src/components/Card.jsx
+++ 04-app/src/components/Card.jsx
@@ -8,22 +8,22 @@ const Card = (props) => {
         <div className="card">
             <div>
                 <div className="top">
-                    <img src='https://thumbs.dreamstime.com/b/amazon-logo-white-background-montreal-canada-july-printed-paper-98221126.jpg'></img>
+                    <img src={props.brand}></img>
                     <button><Bookmark /> Save</button>
                 </div>
                 <div className="center">
-                    <h3>{props.company} <span>3 days ago</span></h3>
-                    <h2>Senior UI/UX designer</h2>
+                    <h3>{props.company} <span>{props.datePosted}</span></h3>
+                    <h2>{props.post}</h2>
                     <div className="tag">
-                        <h4>Part Time</h4>
-                        <h4>Remote</h4>
+                        <h4>{props.tag1}</h4>
+                        <h4>{props.tag2}</h4>
                     </div>
                 </div>
             </div>
             <div className="bottom">
                 <div className="bottom-left">
-                    <h3>$100/hr</h3>
-                    <p>Hyderabad, India</p>
+                    <h3>{props.pay}</h3>
+                    <p>{props.location}</p>
                 </div>
                 <button className="bottom-right">Apply now</button>
             </div>
