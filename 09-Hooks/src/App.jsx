e93c5591dd4ad9421127b87abd9921caf73d5202
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-08 16:13:33 +0530

    Adding the other files

--- 09-Hooks/src/App.jsx
+++ 09-Hooks/src/App.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { useState } from 'react'
 
 const App = () => {
-  const [nums, setNum] = useState('aman')
+  const [nums, setUsers] = useState(['aman ', 'aditya ', 'sanskriti'])
+  const [number, setNums] = useState(67);
   function changeNum(){
-    setNum(69);
+    setNums(69);
+    setUsers(['aditya ', 'subhi ', 'riya']);    
   }
 
 
@@ -13,6 +15,7 @@ const App = () => {
 
     <div>
       <h1>The value of a is {nums}</h1>
+      <h1>The value of number is {number}</h1>
       <button onClick={changeNum}>Click me</button>
     </div>
   )
