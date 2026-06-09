3c86d582e6acba823bb3e887eedaf53952a7b8d5
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-09 22:52:29 +0530

    Commit after dinner

--- 10-useState-Advanced/src/App.jsx
+++ 10-useState-Advanced/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [num, setNum]=useState(10);
+  const btnClicked = () => {
+    console.log(num);
+    setNum(20); // This is a asynchronous process here
+    console.log(num);
+  };
+
+
+
+
+
   return (
     <div>
-      
+      <h1>{num}</h1>
+      <button onClick={btnClicked}>Click nigga</button>
     </div>
   )
 }
