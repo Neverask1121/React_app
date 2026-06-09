5e170781b6e3d43f88374f1aea9b21db163ef5e5
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-09 23:30:05 +0530

    Starting Form Handling

--- 10-useState-Advanced/src/App.jsx
+++ 10-useState-Advanced/src/App.jsx
@@ -1,19 +1,40 @@
 import React, { useState } from 'react'
 
+// const App = () => {
+//   const [num, setNum]=useState(10);
+//   const btnClicked = () => {
+//     console.log(num);
+//     setNum(20); // This is a asynchronous process here
+//     console.log(num);
+//   };
+
+
+
+// new
+
+// const App = () => {
+//   // const [num, setNum] = useState({user:'Sarthak', age:20})
+//   const [num, setNum] = useState([10, 20, 30])
+//   const btnClicked = () => {
+//     const newNum = [...num]
+//     newNum.push(99)
+//     // newNum.user = 'Aman';
+//     // newNum.age = 19;
+//     // setNum(newNum);
+//     setNum(newNum);
+//   }
+
+
 const App = () => {
-  const [num, setNum]=useState(10);
+  const[num, setNum] = useState(10)
   const btnClicked = () => {
-    console.log(num);
-    setNum(20); // This is a asynchronous process here
-    console.log(num);
-  };
-
-
-
-
-
+    setNum(prev => (prev+1))
+    setNum(prev => (prev+1))
+    setNum(prev => (prev+1))
+  }
   return (
     <div>
+      {/* <h1>{num.user}, {num.age}</h1> */}
       <h1>{num}</h1>
       <button onClick={btnClicked}>Click nigga</button>
     </div>
