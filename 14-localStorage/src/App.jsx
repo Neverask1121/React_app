2513d29e7d6bc088d4ce557dba9c97fbc76db5b6
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-12 00:29:37 +0530

    IGU

--- 14-localStorage/src/App.jsx
+++ 14-localStorage/src/App.jsx
@@ -4,12 +4,22 @@ const App = () => {
   
   // localStorage.setItem('user', 'Aditya')
   // localStorage.setItem('age', 18);
-  const age = localStorage.getItem('age');
-  const user = localStorage.getItem('user');
-  localStorage.removeItem('user');
+  // const age = localStorage.getItem('age');
+  // const user = localStorage.getItem('user');
+  // localStorage.removeItem('user');
+  // console.log(user);
+  // console.log(age);
+  // console.log(`User ${user} has the age of ${age}`);
+
+  const user = {
+    username: 'Aditya',
+    age: 18,
+    College: 'IIT DIU'
+  };
+  localStorage.setItem('user', user);
   console.log(user);
-  console.log(age);
-  console.log(`User ${user} has the age of ${age}`);
+
+
   return (
     <div>
       App      
